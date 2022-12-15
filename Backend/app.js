ed593647c2818aa51916bba39bf4ce6a602404aa
amar8961 <amarkumar8961@gmail.com>
2022-12-16 04:49:41 +0530

    app correction

--- Backend/app.js
+++ Backend/app.js
@@ -1,25 +1,25 @@
- const express = require('express')
- const bodyParser = require('body-parser')
- const cors=require('cors')
- const helmet = require('helmet')
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors=require('cors')
+const helmet = require('helmet')
 
- const sequelize=require('./util/database')
- const userRoutes=require('./routes/user')
+const sequelize=require('./util/database')
+const userRoutes=require('./routes/user')
 
- const app=express();
+const app=express();
 
- app.use(cors())
- app.use(helmet())
+app.use(cors())
+app.use(helmet())
 
- app.use(bodyParser.json({extended:false}))
- app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.json({extended:false}))
+app.use(bodyParser.urlencoded({extended:false}))
 
- app.use(userRoutes)
+app.use(userRoutes)
 
- sequelize
+sequelize
 //  .sync({ force: true })
- .sync()
- .then(response=>{
-     console.log(response)
-     app.listen(process.env.PORT , ()=>console.log("Server started running on Port: 3000"))
- }).catch(err=>console.log(err))
+.sync()
+.then(response=>{
+    console.log(response)
+    app.listen(process.env.PORT , ()=>console.log("Server started running on Port: 3000"))
+}).catch(err=>console.log(err))
